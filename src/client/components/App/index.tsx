36eec1e3a83309cf76aa049fcd39571696b0a62e
chrisbmar <50221462+chrisbmar@users.noreply.github.com>
2020-04-23 11:45:49 +0200

    add material-ui

--- src/client/components/App/index.tsx
+++ src/client/components/App/index.tsx
@@ -1,25 +1,44 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
 import React from "react";
 
 import { Switch, Route, Redirect, Link } from "react-router-dom";
-import styles from "./App.scss";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
 
-interface AppProps {}
+import useStyles from "./styles";
 
-export const App: React.FC<AppProps> = ({}) => (
-  <div className={styles.App}>
-    <div>
-      <Link to="/">Home Page</Link>
-      <span> | </span>
-      <Link to="/about">About Page</Link>
+export const App: React.FC = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Button color="inherit">MENU</Button>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+
+      <div>
+        <Link to="/">Home Page</Link>
+        <span> | </span>
+        <Link to="/about">About Page</Link>
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <h1>Home Page</h1>
+        </Route>
+        <Route exact path="/about">
+          <h1>About Page</h1>
+        </Route>
+        <Redirect to="/" />
+      </Switch>
     </div>
-    <Switch>
-      <Route exact path="/">
-        <h1>Home Page</h1>
-      </Route>
-      <Route exact path="/about">
-        <h1>About Page</h1>
-      </Route>
-      <Redirect to="/" />
-    </Switch>
-  </div>
-);
+  );
+};
