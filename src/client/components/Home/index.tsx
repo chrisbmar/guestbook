e766d1f51186eee8d44c4575f0647786d96a2f4b
chrisbmar <50221462+chrisbmar@users.noreply.github.com>
2020-04-23 16:34:19 +0200

    render state from store in home component

--- src/client/components/Home/index.tsx
+++ src/client/components/Home/index.tsx
@@ -1,3 +1,23 @@
 import React from "react";
+import { Card, CardContent, Typography } from "@material-ui/core";
+import { useStoreState } from "../../hooks/index";
 
-export const Home: React.FC = () => <h1>Home</h1>;
+export const Home: React.FC = () => {
+  const entries = useStoreState((state) => state.guestbook.entries);
+
+  return (
+    <div>
+      {entries.map((entry) => (
+        <Card key={entry.content}>
+          <CardContent>
+            <Typography variant="h2">{entry.name}</Typography>
+            <Typography variant="body1">{entry.content}</Typography>
+            <Typography variant="caption">
+              {entry.submitted.toLocaleDateString()}
+            </Typography>
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  );
+};
