6fc53d446e4fbc29bfc08822cb635da46dc9f948
chrisbmar <50221462+chrisbmar@users.noreply.github.com>
2020-04-23 13:30:56 +0200

    home and about components

--- src/client/components/App/index.tsx
+++ src/client/components/App/index.tsx
@@ -4,38 +4,31 @@ import React from "react";
 import { Switch, Route, Redirect, Link } from "react-router-dom";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
+import { Home } from "../Home/index";
+import { About } from "../About/index";
 
 import useStyles from "./styles";
 
 export const App: React.FC = () => {
   const classes = useStyles();
-
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
           <Button color="inherit">MENU</Button>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+          <Button component={Link} to="/" color="inherit">
+            Home
+          </Button>
+          <Button component={Link} to="/about" color="inherit">
+            About
+          </Button>
         </Toolbar>
       </AppBar>
 
-      <div>
-        <Link to="/">Home Page</Link>
-        <span> | </span>
-        <Link to="/about">About Page</Link>
-      </div>
       <Switch>
-        <Route exact path="/">
-          <h1>Home Page</h1>
-        </Route>
-        <Route exact path="/about">
-          <h1>About Page</h1>
-        </Route>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
         <Redirect to="/" />
       </Switch>
     </div>
